6fc990ccba7cb47b5da89b5796cfa34f73feead3
tanjim14 <tanjim2051@gmail.com>
2023-08-22 03:22:56 -0700

    new component Helllo is used in the App conponent

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,16 +1,37 @@
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
+const Hello = () => {
   return(
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a+b}
-      </p>
+      <p>Hello World</p>
     </div>
   )
 }
+
+const App = () => {
+  return(
+    <div>
+      <h1>Greetings</h1>
+      <Hello/>
+    </div>
+  )
+}
+
+
+
+
+
+// const App = () => {
+//   const now = new Date()
+//   const a = 10
+//   const b = 20
+//   console.log(now, a+b)
+//   return(
+//     <div>
+//       <p>Hello world, it is {now.toString()}</p>
+//       <p>
+//         {a} plus {b} is {a+b}
+//       </p>
+//     </div>
+//   )
+// }
 
 export default App
