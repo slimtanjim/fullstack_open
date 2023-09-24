0a3a120217db2d871aacf1ae2097f77469373049
tanjim14 <tanjim2051@gmail.com>
2023-09-24 08:41:13 -0700

    example problems

--- reading_part1/part-b/test-js.js
+++ reading_part1/part-b/test-js.js
@@ -1,5 +1,4 @@
-//array 
-
+//Array 
 const t  = [-1,1,2]
 
 t.push(5)
@@ -8,3 +7,14 @@ console.log(t)
 console.log(t.length)
 console.log(t[0])
 
+t.forEach(value => { console.log(value)})
+
+for(let i in t) { console.log(t[i]) }
+
+
+//Objects 
+
+
+//Functions 
+
+// ex
